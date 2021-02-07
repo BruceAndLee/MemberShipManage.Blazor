6fb090f9dff407b05ce4e09018d86e1b189ca8cb
lilei1986abc <lilei1986abc@163.com>
2021-02-07 19:16:54 +0800

    update

---
+++ MemberShipManage.Infrastructure/Extension/DialogCloseEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberShipManage.Infrastructure.Extension
+{
+    public class DialogCloseEventArgs : EventArgs
+    {
+        public bool DialogResult { get; set; }
+
+        public string ReturnMessage { get; set; }
+    }
+}
